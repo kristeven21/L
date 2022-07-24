f90e2a08df868c6a32e940e2b8835ec445de2daa
Lucy Harun <108619102+lucyharun@users.noreply.github.com>
2022-07-24 15:36:50 +0800

    Update __main__.py

--- wbb/__main__.py
+++ wbb/__main__.py
@@ -118,13 +118,13 @@ home_keyboard_pm = InlineKeyboardMarkup(
                 text="All Commands❓", callback_data="bot_commands"
             ),
             InlineKeyboardButton(
-                text="Report Issue🛠",
-                url="https://t.me/asinpeda",
+                text="Repository🛠",
+                url="https://github.com/lucyharun/bissmillah/",
             ),
         ],
         [
             InlineKeyboardButton(
-                text="Bot Stats💻",
+                text="Bot Stats🤖",
                 callback_data="stats_callback",
             ),
             InlineKeyboardButton(
@@ -155,7 +155,7 @@ keyboard = InlineKeyboardMarkup(
             ),
             InlineKeyboardButton(
                 text="Repo 🛠",
-                url="https://github.com/lucyharun/",
+                url="https://github.com/lucyharun/bissmillah/",
             ),
         ],
         [
